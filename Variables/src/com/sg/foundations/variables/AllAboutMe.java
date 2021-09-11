5e1b15b49eb803e984aae03d61872b5f822907d7
Arjun Panyam <43484584+panyama1@users.noreply.github.com>
2021-09-10 22:05:08 -0500

    Add files via upload

---
+++ Variables/src/com/sg/foundations/variables/AllAboutMe.java
@@ -0,0 +1,17 @@
+package com.sg.foundations.variables;
+
+public class AllAboutMe {
+    public static void main(String[] args) {
+        String name = "Arjun";
+        String food = "raviolli";
+        int pet_num = 5;
+        String domicile = "house";
+        String ability = "walk";
+
+        System.out.println("My name is "+name);
+        System.out.println("My favorite food is "+food);
+        System.out.println("I have "+pet_num+"pets");
+        System.out.println("I live in a "+domicile+" and it's nice.");
+        System.out.println("It is true I know how to "+ability);
+    }
+}
